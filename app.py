072146e53d7085b2a5da01ffb602b74444673313
Nishant Sharma <nishant.1511977@gmail.com>
2025-11-01 22:48:21 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
+# Load your ML model
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -17,4 +19,6 @@ def predict():
     return render_template('index.html', prediction_text=f'Recommended Crop: {prediction[0]}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # required for Render
+    app.run(host='0.0.0.0', port=port)
+
